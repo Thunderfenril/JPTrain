883faca2598f7106d19198b52ec9269671b74717
thunderfenril <nicolasguyomarch67@gmail.com>
2025-08-01 09:12:37 +0200

    Feature: Creation of a menu in cli

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,38 @@
-export function main() : void {
-    console.log("Hello world");
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function askQuestion(query: string): Promise<string> {
+  return new Promise(resolve => {
+    rl.question(query, resolve);
+  });
 }
-main();
+
+export async function main() : Promise<void> {
+    console.log("What do you wish to do ?\n1) Review Hiragana\n2)Review Katagana\n3)Review Kanji");
+
+    while(true) {
+        const choice = await askQuestion("Choose an option (1-3): ");
+    switch (choice.trim()) {
+    case '1':
+      console.log("Hiragana WIP");
+      rl.close();
+      return;
+    case '2':
+      console.log("Katagana WIP");
+      rl.close();
+      return;
+    case '3':
+      console.log("Kanji WIP");
+      rl.close();
+      return;
+    default:
+      console.log("Invalid option. Please try again.\n");
+      break;
+    }
+    }
+}
+main()
